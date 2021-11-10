355b32428b47f10a3ed591748b1052917ff62964
Alex Sung <alexsung98@gmail.com>
2021-11-10 01:28:22 -0800

    env client id and secret obtained by .env file hidden
    update readme, start authorization implementation

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
 
 // a simple route handler for the index page
 // tells express app to handle GET request on homepage (index) route
